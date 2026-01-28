19befa28bfdac9e5b20936a5eca4f6fd5b82a46c
Luigi The <luigigithub@gmail.com>
2026-01-27 21:03:40 -0300

    i fucked up lemme fix that very quickly

--- src/main/kotlin/com/luigicxv711/x64em/Hardware/Ports/VGAPort.kt
+++ src/main/kotlin/com/luigicxv711/x64em/Hardware/Ports/VGAPort.kt
@@ -3,11 +3,11 @@ package com.luigicxv711.x64em.Hardware.Ports
 import com.luigicxv711.x64em.Hardware.CPU.CPU
 
 class VGAPort(cpu: CPU) : Port(cpu) {
-    override fun write(port: Int, data: UByteArray) {
+    override fun write(port: Int, data: ByteArray) {
         TODO("Not yet implemented")
     }
 
-    override fun read(): UByteArray {
+    override fun read(port: Int): ByteArray {
         TODO("Not yet implemented")
     }
 }
